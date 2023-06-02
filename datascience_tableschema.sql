7e6ac361606a7d9c57de08928a7d528c455401ea
Kelsey Abbey <kelseyabbey2@gmail.com>
2023-06-01 20:29:00 -0600

    initial commit for data

---
+++ datascience_tableschema.sql
@@ -0,0 +1,24 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/ZDXqsA
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION QUICKDBD
+
+CREATE TABLE [data_science] (
+    [work_year] int  NOT NULL ,
+    [experience_level] varchar(5)  NOT NULL ,
+    [employment_type] varchar(5)  NOT NULL ,
+    [job_title] varchar(50)  NOT NULL ,
+    [salary] int  NOT NULL ,
+    [salary_currency] varchar(3)  NOT NULL ,
+    [salary_in_usd] int  NOT NULL ,
+    [employee_residence] varchar(2)  NOT NULL ,
+    [remote_ratio] int  NOT NULL ,
+    [company_location] varchar(2)  NOT NULL ,
+    [company_size] varchar(1)  NOT NULL 
+)
+
+COMMIT TRANSACTION QUICKDBD
