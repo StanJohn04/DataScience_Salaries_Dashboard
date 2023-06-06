88265e107962654fc9038cc2788bda54e597f3e7
jenntruly <lilmzcandybar@gmail.com>
2023-06-05 21:25:07 -0600

    map views

--- static/map.js
+++ static/map.js
@@ -14,7 +14,13 @@ var myMap = L.map("map", {
 
 
   //All other map code goes here
-
+// Define variables for our tile layers.
+var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
+var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
   // Layers
 
   //Markers
