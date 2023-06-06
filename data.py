504ed1fa0f51d7b9198cd7c2c445d6d4dc00556e
Brian Haynes <122308689+brianphaynes@users.noreply.github.com>
2023-06-05 21:25:35 -0600

    Adding data to pull from

---
+++ data.py
@@ -0,0 +1,28 @@
+import sqlite3
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    #print request recieved to terminal
+    print(f"Server recieved request for index route")
+    return "It worked!"
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    conn = sqlite3.connect("my_data.db")
+    c = conn.cursor()
+    data = c.execute('''SELECT * FROM data_salaries''').fetchall()
+    conn.close()
+
+    # Convert the data to a list of dictionaries
+    column_names = [description[0] for description in c.description]
+    data_dict = [dict(zip(column_names, row)) for row in data]
+
+    # Return the data as JSON
+    return jsonify(data_dict)
+
+if __name__ == '__main__':
+    app.run()
