f6f1bf3a3a1b2c4a44c9cf6072c7a3bd14199804
jenntruly <lilmzcandybar@gmail.com>
2023-06-06 21:18:54 -0600

    added toggle

--- static/map.js
+++ static/map.js
@@ -5,13 +5,25 @@ var myMap = L.map("map", {
     center: [45.52, -122.67],
     zoom: 4
   });
+  // data 
+let url = 'http://127.0.0.1:5000/data'
+d3.json(url).then(function(data) {
+  console.log(data);
+
+  let countryList = [];
+  for (let i = 0; i < data.length; i++) {
+    let countryCode = data[i].company_location;
+    if (countryList.indexOf(countryCode) === -1) {
+      countryList.push(countryCode);
+    }
+  }
   
+});
   // Adding a tile layer (the background map image) to our map:
   // We use the addTo() method to add objects to our map.
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(myMap);
-
 
   //All other map code goes here
 // Define variables for our tile layers.
@@ -20,10 +32,18 @@ var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 })
 var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-});
-  // Layers
+})
 
-  //Markers
+// Only one base layer can be shown at a time.
+var baseMaps = {
+  Street: street,
+  Topography: topo
+};
+
+// Pass our map layers to our layer control.
+// Add the layer control to the map.
+L.control.layers(baseMaps).addTo(myMap);
+
 
   //etc.
 
@@ -31,3 +51,5 @@ var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
   
   
   
+
+
